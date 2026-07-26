5f81a85d35c4d48a75d7324543c9d3fe64b857d0
sahanaka2004-ka <sahanaka2004@gmail.com>
2026-07-26 17:29:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,69 @@
 let blogs = JSON.parse(localStorage.getItem("blogs")) || [];
 
 function displayBlogs() {
+
     const blogContainer = document.getElementById("blogPosts");
 
     blogContainer.innerHTML = "";
-    
+
     blogs.forEach(blog => {
+
         blogContainer.innerHTML += `
-            <div class="blog">
-                <h3>${blog.title}</h3>
-                <p><b>Author:</b> ${blog.author}</p>
-                <p>${blog.content}</p>
-            </div>
+        
+        <div class="blog">
+
+            <h3>${blog.title}</h3>
+
+            <p>${blog.content}</p>
+
+            <p><b>Author:</b> ${blog.author}</p>
+
+        </div>
+
         `;
+
     });
 }
+
 
 function addBlog() {
 
     let title = document.getElementById("title").value;
     let author = document.getElementById("author").value;
     let content = document.getElementById("content").value;
-    if(title==="" || author==="" || content===""){
-    alert("Please fill all fields");
-    return;
-}
+
+
+    if(title === "" || author === "" || content === "") {
+
+        alert("Please fill all fields");
+        return;
+
+    }
+
 
     let blog = {
+
         title: title,
         author: author,
         content: content
+
     };
+
 
     blogs.push(blog);
 
+
     localStorage.setItem("blogs", JSON.stringify(blogs));
 
+
     displayBlogs();
+
 
     document.getElementById("title").value = "";
     document.getElementById("author").value = "";
     document.getElementById("content").value = "";
+
 }
+
 
 displayBlogs();
